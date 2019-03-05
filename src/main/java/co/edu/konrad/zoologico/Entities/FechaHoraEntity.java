2147af162c9d236d04d0fd1a760af59356d69c03
jack-down <44452000+jack-down@users.noreply.github.com>
2019-03-04 22:20:01 -0500

    Adicción fecha y hora
    Se agrega fecha y hora, con una id propia (Consultar como hacer ids compuestas)

---
+++ src/main/java/co/edu/konrad/zoologico/Entities/FechaHoraEntity.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.zoologico.Entities;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+/**
+ *
+ * @author Sergio S. Salcedo T 
+ */
+@Entity
+@Table(name="Fecha_Hora")
+public class FechaHoraEntity implements Serializable {
+    /**
+     * Llave primaria temporal mientras descubrimos como hacer llaves compuestas
+     */
+     @Id
+    @Column(name = "id_Fecha_Hora")
+   @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long idFecHo;
+     /**
+      * Attr: Id del lugar
+      */
+     @ManyToOne
+     @JoinColumn(name = "id_lugar")
+     private LugarEntity idLugar;
+     /**
+      * Attr: fecha
+      */
+     @Column
+     private Date fecha;
+     /**
+      * Attr: hora
+      */
+     @Column
+     private Time hora;
+
+    public FechaHoraEntity() {
+    }
+
+    public Long getIdFecHo() {
+        return idFecHo;
+    }
+
+    public LugarEntity getIdLugar() {
+        return idLugar;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setIdFecHo(Long idFecHo) {
+        this.idFecHo = idFecHo;
+    }
+
+    public void setIdLugar(LugarEntity idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+     
+     
+}
---
+++ src/main/java/co/edu/konrad/zoologico/persistence/FechaHoraPersistence.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.zoologico.persistence;
+
+import co.edu.konrad.zoologico.Entities.FechaHoraEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+/**
+ *
+ * @author Sergio S. Salcedo T 
+ */
+@Stateless
+public class FechaHoraPersistence {
+     /*
+    * Declaracion manejador
+    */
+    @PersistenceContext(unitName = "zoologicoPU")
+    protected EntityManager em;
+    
+   /**
+    * Metodo para insertar una fecha y hora
+    * @param a
+    * @return 
+    */
+    
+    public FechaHoraEntity insert(FechaHoraEntity a){
+        em.persist(a);
+        return a;
+    }
+    
+    /*
+    * Metodo para listar todos las fechas y horas
+    */
+    
+    public List<FechaHoraEntity> findAll(){
+        Query results = em.createQuery("select a from FechaHoraEntity a");
+        return results.getResultList();
+    }
+    
+    /**
+     * Metodo para encontrar por id
+     * @param id
+     * @return 
+     */
+    public FechaHoraEntity find(int id){
+        return em.find(FechaHoraEntity.class, id);
+    }
+    
+    /*
+    * Metodo para borrar una fecha y hora segun id
+    * @param id
+    */
+    public void delete(int id){
+        FechaHoraEntity entity = em.find(FechaHoraEntity.class,id);
+        em.remove(entity);
+    }
+    
+    /**
+     * metodo para actualizar una fecha y hora
+     * @param z
+     * @return 
+     */
+    public FechaHoraEntity update(FechaHoraEntity z){
+        return em.merge(z);
+    }
+    
+   
+}
