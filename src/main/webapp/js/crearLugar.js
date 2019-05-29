f244558aa7a162801798704d9ddbb16823726e55
Sergio Rodriguez <32339485+checholini@users.noreply.github.com>
2019-05-29 10:15:16 -0500

    Creacion CRUD en show para lugar

---
+++ src/main/webapp/js/crearLugar.js
@@ -0,0 +1,76 @@
+(function ($) {
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName [1] === undefined ? true : sParameterName[1];
+            }
+        }
+    }; 
+
+    if (getUrlParameter('id')) {
+        $.ajax({
+            method: 'GET',
+            //Cambiar Ruta al servicio necesitado
+            url: '/ZoologicoNuevasTecnologias-develop/api/lugar/' + getUrlParameter('id'),
+            contentType: 'application/json',
+            dataType: 'json'
+        }).done(function (data) {
+            //Crear variables con los camnpos de la entidad y referenciar los campos en el formulario
+            var nomLugar = $('#nomLugar').val(data.nomLugar);
+            var direccionLugar = $('#direccionLugar').val(data.direccionLugar);
+            var id = data.idLugar;
+            $('#CrearBoton').text("Actualizar").click(function (event) {
+                $.ajax({
+                    method: 'PUT',
+                    //Cambiar Ruta al servicio necesitado
+                    url: '/ZoologicoNuevasTecnologias-develop/api/lugar/' + id,
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    //Mapear datos si hay un json dentro de otro, añadir "id": {"id" : valorId} 
+                    data: JSON.stringify({
+                        idLugar:id,
+                        nomLugar: direccionLugar.val(),
+                        direccionLugar: nomLugar.val()
+                    })
+                }).done(function (data) {
+                    window.location.href = '/ZoologicoNuevasTecnologias-develop';
+                }).fail(function (xhr, status, error) {
+                    console.log.error;
+                });
+            });
+
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    } else {
+        $('#CrearBoton').click(function (event) {
+            //Crear variables con los camnpos de la entidad y referenciar los campos en el formulario
+            var nomLugar = $('#nomLugar').val();
+            var direccionLugar = $('#direccionLugar').val();
+            //Mapear datos si hay un json dentro de otro, añadir "id": {"id" : valorId}
+            $.ajax({
+                method: 'POST',
+                 //Cambiar Ruta al servicio necesitado
+                url: '/ZoologicoNuevasTecnologias-develop/api/lugar/',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                        nomLugar: direccionLugar,
+                        direccionLugar: nomLugar 
+                })
+            }).done(function (data) {
+                window.location.href = '/ZoologicoNuevasTecnologias-develop';
+            }).fail(function (xhr, status, error) {
+                console.log(error);
+            });
+        });
+    }
+
+
+
+})(jQuery);
