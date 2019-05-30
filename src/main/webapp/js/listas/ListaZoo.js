1c058695fe79ebea10369f1f0573227849b48940
jack-down <44452000+jack-down@users.noreply.github.com>
2019-05-30 18:27:42 -0500

    Listas de valores

---
+++ src/main/webapp/js/listas/ListaZoo.js
@@ -0,0 +1,22 @@
+(function ($) {
+    $(document).ready(function (event) {
+        $.ajax({
+            method: 'GET',
+            url: '/ZoologicoNuevasTecnologias/api/zoo',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+              var id = data[i].idZoo;
+              var nombre =data[i].nomZoo;
+              var option = "<option value=" + id + ">" + nombre + "</option>";
+              console.log(option);
+              $( "#zoo" ).append(option);
+            }
+            console.log(data);
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+
+})(jQuery);
